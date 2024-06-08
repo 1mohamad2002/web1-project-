0c8063cc51cafe87802cc1cdd851a9d2d76fbd19
mohamad saad sukkar <80129582+1mohamad2002@users.noreply.github.com>
2024-06-08 19:59:03 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+/*window.onload = function () {
+    alert("Hello! we add new recipes every week " );
+}
+*/
+function changeStyle(bgColor, textColor) {
+    var paragraph = document.getElementById('p');
+    paragraph.style.backgroundColor = bgColor;
+    paragraph.style.color = textColor;
+}
